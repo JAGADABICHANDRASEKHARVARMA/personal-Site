db0fd1a22ddbbbacef3ae1b60d5f81d165b3c74a
JAGADABI CHANDRASEKHAR VARMA <68846646+JAGADABICHANDRASEKHARVARMA@users.noreply.github.com>
2021-02-09 22:34:49 +0530

    first commit

---
+++ js/typewriter.js
@@ -0,0 +1,7 @@
+var app = document.getElementById('typewriter');
+
+var typewriter = new Typewriter(app, {
+    strings : ['Full Stack Developer','DevOps Enthusiast', 'Digital Marketing Stragiest'],
+    autoStart: true,
+    loop: true
+});
