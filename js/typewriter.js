47328cf21ca1b19fb2926a3e89bb19b97dffd2f1
JAGADABI CHANDRASEKHAR VARMA <68846646+JAGADABICHANDRASEKHARVARMA@users.noreply.github.com>
2021-08-26 19:42:45 +0530

    Update typewriter.js

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,7 +1,7 @@
 var app = document.getElementById('typewriter');
 
 var typewriter = new Typewriter(app, {
-    strings : ['Full Stack Developer','DevOps Enthusiast', 'Digital Marketing Stragiest'],
+    strings : ['Full Stack Developer','UI/UX Designer', 'Digital Marketing Stragiest'],
     autoStart: true,
     loop: true
 });
